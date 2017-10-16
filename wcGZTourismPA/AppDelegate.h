9d8f9e8006d12b6ff7d47f54ca7f94d08dc1cb51
wangxiaola <1015559188@qq.com>
2017-10-16 15:52:36 +0800

    321

---
+++ wcGZTourismPA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wcGZTourismPA
+//
+//  Created by 王小腊 on 2017/10/16.
+//  Copyright © 2017年 王小腊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
